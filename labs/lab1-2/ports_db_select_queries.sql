5b027fee33614c61982780a4a6d32e61ad7454f8
Alexey <89032904807alek@gmail.com>
2023-05-27 03:58:56 +0300

    Implemented lab1-2

---
+++ labs/lab1-2/ports_db_select_queries.sql
@@ -0,0 +1,12 @@
+SELECT Name from ships;
+
+SELECT Name from ships where CapitanID = 4;
+
+SELECT Name, Surname, Age FROM passenger order by Age;
+
+SELECT max(age) FROM passenger GROUP BY ShipID;
+
+SELECT DISTINCT(CapitanID) FROM Ships;
+
+SELECT (2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
+
