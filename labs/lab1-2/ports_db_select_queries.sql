d51cf957e2e1a7e48a55563c5f51267b1eb3c39b
Alexey <89032904807alek@gmail.com>
2023-05-27 05:06:40 +0300

    Added more queries for lab1-2

--- labs/lab1-2/ports_db_select_queries.sql
+++ labs/lab1-2/ports_db_select_queries.sql
@@ -4,9 +4,13 @@ SELECT Name from ships where CapitanID = 4;
 
 SELECT Name, Surname, Age FROM passenger order by Age;
 
-SELECT max(age) FROM passenger GROUP BY ShipID;
+SELECT max(Age) FROM passenger GROUP BY ShipID;
 
 SELECT DISTINCT(CapitanID) FROM Ships;
+
+SELECT Name, PortID, RouteID FROM Ships;
+
+SELECT avg(Age) FROM passenger WHERE ShipID = 1;
 
 SELECT (2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
 
